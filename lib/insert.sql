42765f1dc52028473630b2928e252167f060ff5e
Santiago Sanchez <chagostwo@yahoo.com>
2019-07-19 02:36:57 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('Maru', 3, 'male','grey','chill','true');
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('rusty', 4, 'female','black','moody','true');
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('bloop', 100, 'male','orange','chill','false');
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('burt', 50, 'male','purple','aggressive','false');
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('Mart', 7, 'female','black','quiet','true');
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('hut', 6, 'male','brown','loud','true');
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('chimp', 3, 'female','red','energetic','true');
+INSERT INTO bears (name, age,gender,color ,temperament,alive ) VALUES ('Mut', 9, 'male','grey','lazy','true');
